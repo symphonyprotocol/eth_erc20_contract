8c4e75844e223e986f7683a23dcfde8def7d620f
Andy Cui <strawhat2010@live.cn>
2018-12-21 16:02:31 +0800

    internal: enabled ownership transfer.

--- contracts/SymphonyProtocol.sol
+++ contracts/SymphonyProtocol.sol
@@ -13,11 +13,16 @@ contract SymphonyProtocol is ERC20, ERC20Detailed, Ownable {
     _mint(msg.sender, 10000000000 * 10 ** uint(decimals()));
   }
 
-  function batchTransfer(address[] addresses, uint256[] amounts) public returns (bool) {
+  function bulkTransfer(address[] addresses, uint256[] amounts) public returns (bool) {
     require(addresses.length == amounts.length);
     for (uint i = 0; i < addresses.length; i++) {
       _transfer(msg.sender, addresses[i], amounts[i]);
     }
     return true;
+  }
+
+  function transferOwnership(address newOwner) public onlyOwner {
+    super.transferOwnership(newOwner);
+    _transfer(msg.sender, newOwner, balanceOf(msg.sender));
   }
 }
